8eacee8971c9c04be2221a75e7a095af3bf0cf05
Alemshewit <alemshewitweldearegay@gmail.com>
2014-09-23 12:32:58 -0600

    highestscore

--- Hang Man/Hang Man/Program.cs
+++ Hang Man/Hang Man/Program.cs
@@ -165,6 +165,49 @@ namespace Hang_Man
                     Console.WriteLine();
                 }
             }
+
+            static void HighScores(int playerScore)
+            {
+                Console.WriteLine("Your name:");
+                string playerName = Console.ReadLine();
+
+                AlemEntities db = new AlemEntities();
+
+                HighestScore newHighestScore = new HighestScore();
+                newHighestScore.DateCreated = DateTime.Now;
+                newHighestScore.Game = "Guess That Number";
+                newHighestScore.Name = playerName;
+                newHighestScore.Score = playerScore;
+
+                db.HighestScores.Add(newHighestScore);
+
+                db.SaveChanges();
+            }
+
+            static void DisplayHighestScore()
+            {
+                Console.Clear();
+                Console.WriteLine("Guess That Number High Scores");
+                Console.WriteLine("*****************************");
+
+                AlemEntities db = new AlemEntities();
+                List<HighestScore> highestScoreList = db.HighestScores
+                    .Where(x => x.Game == "Guess That Number")
+                    .OrderBy(x => x.Score).Take(10)
+                    .ToList();
+
+                foreach (HighestScore highScore in highestScoreList)
+                {
+                    Console.WriteLine("{0}. {1} - {2} on {3}",
+                        highestScoreList.IndexOf(highScore) + 1,
+                        highScore.Name,
+                        highScore.Score,
+                        highScore.DateCreated.Value.ToShortDateString());
+                }
+                Console.WriteLine("\n\n\n");
+
+
+            }
     }
        
 
